534fed632432eff414d5f975606f844597c033b2
prince1565 <kumar.prince832002@gmail.com>
2024-06-08 16:04:44 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import WeatherDisplay from './WeatherDisplay';
 import SearchForm from './SearchForm';
-
+import Spinner from './Spinner';
 
 
 export default function Home(props) {
@@ -12,6 +12,7 @@ const mode1=props.mode;
 
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
+  const [spinner, setspinner] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -27,7 +28,9 @@ const mode1=props.mode;
 
   const fetchWeatherDataByCoords = async (lat, lon) => {
     try {
+      setspinner(true);
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+      setspinner(false);
       setWeatherData(response.data);
       setError(null);
     } catch (err) {
@@ -37,7 +40,9 @@ const mode1=props.mode;
 
   const fetchWeatherData = async (loc) => {
     try {
+      setspinner(true);
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${API_KEY}&units=metric`);
+      setspinner(false);
       setWeatherData(response.data);
       setError(null);
     } catch (err) {
@@ -71,6 +76,9 @@ const mode1=props.mode;
       <SearchForm onSearch={handleSearch} />
       <div className="col-12 col-md-6 mx-auto mt-4">
       {error && <h5 className="error">{error}</h5>}
+      </div>
+      <div className="col-12 col-md-6 mx-auto mt-4">
+      {spinner===true?<Spinner/>:""}
       </div>
       <div className="col-sm-12 my-4">
       {weatherData && <WeatherDisplay data={weatherData} mode={props.mode}/>}
---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import spinner from '../imges/loading.gif'
+export default function Spinner() {
+  return (
+    <div>
+        <img src={spinner} alt='Loading'/>
+    </div>
+  )
+}
